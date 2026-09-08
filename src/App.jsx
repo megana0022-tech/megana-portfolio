0e70a69db8ee3eb36ec4cec6b0541637b5aee1bf
Megana K <268859307+megana0022-tech@users.noreply.github.com>
2026-09-08 14:44:35 +0530

    Initial React portfolio foundation

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>Megana Portfolio</h1>
+      <p>Digital experiences are coming.</p>
+    </main>
+  )
+}
+
+export default App
